4c6bfe6eac1d5fa3691ec144409bafe39d5f94c3
mtao <jmtao33@gmail.com>
2015-10-22 15:55:12 +0800

    增加tcp框架

---
+++ src/GatewayWorker/Config.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace Utils\GatewayWorker;
+use Utils\GatewayWorker\Config\Store;
+use Closure;
+
+class Config
+{
+    public static function initialize(Closure $initializer)
+    {
+        $class_name = get_called_class();
+        $initializer(new $class_name());
+    }
+
+    public function setDriver($driver)
+    {
+        Store::$driver = $driver;
+    }
+
+    public function setGateway($gateway)
+    {
+        Store::$gateway = $gateway;
+    }
+
+    public function setStorePath($path = '')
+    {
+        Store::$storePath = !$path ?
+            sys_get_temp_dir().'/workerman-sender/' :
+            $path;
+    }
+}
